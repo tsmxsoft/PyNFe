1020a937f03a395077ab3a7e67be78be6ea0abd3
Thiago Montenegro <opencode@gmail.com>
2020-12-09 17:37:28 -0300

    no message

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements(f):
 
 setup(
     name='PyNFe',
-    version='0.4.22',
+    version='0.4.23',
     packages=find_packages(),
     package_data={
         'pynfe': ['data/**/*.txt','data/**/*.xsd'],
