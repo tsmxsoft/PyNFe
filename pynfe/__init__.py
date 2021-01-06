2cacd6ad8cd8b5f05db230849911b3e515cf5549
Thiago Montenegro <opencode@gmail.com>
2021-01-06 18:10:35 -0300

    no message

--- pynfe/__init__.py
+++ pynfe/__init__.py
@@ -1,5 +1,5 @@
 def get_version():
-    return '0.4.26'
+    return '0.4.27'
 
 __version__ = get_version()
 __author__ = 'Marinho Brandao, Junior Tada, Leonardo Tada, Thiago O Lindo'
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def requirements(f):
 
 setup(
     name='PyNFe',
-    version='0.4.26',
+    version='0.4.27',
     packages=find_packages(),
     package_data={
         'pynfe': ['data/**/*.txt','data/**/*.xsd'],
