ff14af78fa21660a27fb34de8d2c84ee74c276e0
Junior Andrade <seisxis@gmail.com>
2023-06-29 10:22:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def requirements(f):
 
 setup(
     name='PyNFe',
-    version='0.4.70',
+    version='0.4.71',
     packages=find_packages(),
     package_data={
         'pynfe': ['data/**/*.txt','data/**/*.xsd'],
