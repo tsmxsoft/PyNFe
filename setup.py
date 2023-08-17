7e43dcb9f98bdb507c0b08b4fc277add39e8500c
christian-tsmx <137795700+christian-tsmx@users.noreply.github.com>
2023-08-17 14:58:09 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def requirements(f):
 
 setup(
     name='PyNFe',
-    version='0.4.82',
+    version='0.4.83',
     packages=find_packages(),
     package_data={
         'pynfe': ['data/**/*.txt','data/**/*.xsd'],
