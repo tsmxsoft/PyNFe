bb6414a84e46d18750b1f98c5bb17b12247955e5
Junior Andrade <seisxis@gmail.com>
2023-11-21 14:43:01 -0300

    icms30

--- pynfe/processamento/serializacao.py
+++ pynfe/processamento/serializacao.py
@@ -366,6 +366,10 @@ class SerializacaoXML(Serializacao):
                         etree.SubElement(icms_item, 'pICMS').text = '{:.2f}'.format(produto_servico.icms_aliquota or 0)          # Alíquota do imposto
                         etree.SubElement(icms_item, 'vICMS').text = '{:.2f}'.format(produto_servico.icms_valor or 0) # Valor do ICMS
                     # 10=Tributada e com cobrança do ICMS por substituição tributária
+                    elif produto_servico.icms_modalidade == '30':
+                        etree.SubElement(icms_item, 'vBCST').text = str(produto_servico.icms_st_valor_base_calculo or 0)
+                        etree.SubElement(icms_item, 'pICMSST').text = str(produto_servico.icms_st_aliquota or 0)
+                        etree.SubElement(icms_item, 'vICMSST').text = str(produto_servico.icms_st_valor or 0)
                     elif produto_servico.icms_modalidade == '10':
                         etree.SubElement(icms_item, 'vBC').text = '{:.2f}'.format(produto_servico.icms_valor_base_calculo or 0)  # Valor da BC do ICMS
                         etree.SubElement(icms_item, 'pICMS').text = '{:.2f}'.format(produto_servico.icms_aliquota or 0)          # Alíquota do imposto
