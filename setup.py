1e193ea27a99f4d3da6aa7ffeb8e2a84944aa016
christian-tsmx <137795700+christian-tsmx@users.noreply.github.com>
2024-10-04 13:12:47 -0300

    version up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyNFe',
-    version='0.4.108',
+    version='0.4.109',
     packages=find_packages(),
     package_data={
         'pynfe': ['data/**/*.txt','data/**/*.xsd'],
